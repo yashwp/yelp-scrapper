7d46ee02813661d91446f39d7b0ad85488f2a248
Yashwardhan Pauranik <pauranik51@gmail.com>
2019-12-09 23:06:25 +0530

    feat: Saving reviews and rating to DynamoDB done

--- utils/saveRatings.js
+++ utils/saveRatings.js
@@ -1,3 +1,24 @@
-module.exports = (obj) => {
-  console.log(obj, 'connecting to DynamoDB');
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+const db = new AWS.DynamoDB.DocumentClient;
+module.exports = (obj, businessName) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      businessName,
+      scrapedAt: JSON.stringify(new Date()),
+      rating: obj.rating,
+      reviewCount: obj.reviewCount
+    }
+  };
+  
+  db.put(params, err => {
+    if (err) {
+      console.error(`Error in adding data ${JSON.stringify(err)}`);
+      return Promise.reject();
+    }
+    
+    return Promise.resolve(params.Item);
+  });
 };
